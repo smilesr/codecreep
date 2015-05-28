fddcf726676573945a8dc208c0df1c97c199607a
Robert Smiles Jr <smilesr120@gmail.com>
2015-05-28 08:48:42 -0400

    some initial code but did not get far

--- lib/codecreep.rb
+++ lib/codecreep.rb
@@ -5,14 +5,25 @@ require 'pry'
 require 'codecreep/init_db'
 require 'codecreep/github'
 require "codecreep/version"
-
+require 'codecreep/user'
 
 module Codecreep
   class App
     def initialize
       @github = Github.new
+      @friends_info = friends_info = 0
     end
+
+    def run
+      puts "what are the names of the users that you want to compare (separate each user name with a comma (no spaces)"
+      users = gets.chomp.split(',')
+      users.each do |user|
+        users = User.first_or_create[user]
+      end
+    end
+
   end
+  binding pry
 end
 
 # app = Codecreep::App.new
--- lib/codecreep/github.rb
+++ lib/codecreep/github.rb
@@ -5,5 +5,35 @@ module Codecreep
     include HTTParty
     base_uri 'https://api.github.com'
     basic_auth ENV['GH_USER'], ENV['GH_PASS']
+
+User.first_or_create
+
+  # GET /users/:username
+  def get_users(user_name) 
+      self.get("users/#{user_name}")
+  end
+
+  #GET /users/:username/followers
+  def get_followers(user_name)
+      Github.get("/users/#{user_name}/followers")
+  end
+
+  # GET /users/:username/following
+  def get_followees(username)
+      Github.get("/users/#{user_name}/following")
+  end
+
+  def 
+    users.each do |key, value|
+      value.values.map do |v|
+        user_specific = [key, v]
+      end
+    end
+  end
+end
+
+
+    
+  end
   end
 end
